a7673c94926206cc687445ed7a1e5bd1f9979ae6
elvish-ishaan <ishaanelvish@gmail.com>
2025-10-14 19:51:04 +0530

    chore: migrated from qdrant to pinecone

--- lib/core/vectorStore.ts
+++ lib/core/vectorStore.ts
@@ -1,9 +1,27 @@
+// import { embeddings } from "./embedings";
+// import { QdrantVectorStore } from "@langchain/qdrant";
+
+
+// export const vectorStore = await QdrantVectorStore.fromExistingCollection(embeddings, {
+//   url: process.env.QDRANT_URL!,
+//   apiKey: process.env.QDRANT_API_KEY!,
+//   collectionName: "askguru-testing",
+// });
+
+
+import { PineconeStore } from "@langchain/pinecone";
+
+import { Pinecone as PineconeClient } from "@pinecone-database/pinecone";
 import { embeddings } from "./embedings";
-import { QdrantVectorStore } from "@langchain/qdrant";
 
+const pinecone = new PineconeClient();
+// Will automatically read the PINECONE_API_KEY and PINECONE_ENVIRONMENT env vars
+const pineconeIndex = pinecone.Index(process.env.PINECONE_INDEX!);
 
-export const vectorStore = await QdrantVectorStore.fromExistingCollection(embeddings, {
-  url: process.env.QDRANT_URL!,
-  apiKey: process.env.QDRANT_API_KEY!,
-  collectionName: "askguru-testing",
+export const vectorStore = await PineconeStore.fromExistingIndex(embeddings, {
+  pineconeIndex,
+  // Maximum number of batch requests to allow at once. Each batch is 1000 vectors.
+  maxConcurrency: 5,
+  // You can pass a namespace here too
+  // namespace: "foo",
 });
